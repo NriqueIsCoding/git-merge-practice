fd11e971601ec3146334fdee91ae0fd2f04405f0
Frank Ruiz <sanreynaldo@gmail.com>
2018-01-11 11:52:24 -0500

    Added a new route and some comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'cars#deals'
+
+#this code is for them deals in them routes
+
+
 end
